7e13160679133fdd3eacd51b5f0042979309bcea
fxj <1753262812@qq.com>
2022-03-14 10:38:01 +0800

    sixth

--- src/componet/partySize/PartySize.js
+++ src/componet/partySize/PartySize.js
@@ -1,34 +1,60 @@
 import React from 'react';
 import './PartySize.css';
-import {Routes , Route ,NavLink} from 'react-router-dom' 
+import { NavLink } from 'react-router-dom' 
 
 class NumberKeyboard extends React.Component{
+  
+  state={
+    count:'0',
+  }
 
+  countChange=(e)=>{
+    if(this.state.count==0){
+      this.setState({count:e.target.innerHTML});
+    }else{
+      const newCount=this.state.count+e.target.innerHTML;
+      this.setState({count:newCount});
+    }
+  }
+
+  countDelete=(e)=>{
+    if(this.state.count==0){
+      return;
+    }else{
+       if(this.state.count.length==1){
+          this.setState({count:0})
+       }else{
+          const newCount=this.state.count.slice(0,[this.state.count.length-1])
+          this.setState({count:newCount});
+       }     
+    }
+  }
+  
   render(){
     return(
       <div className="numberKeyboard">
-         <div className="inputResult">0</div>
+         <div className="inputResult">{this.state.count}</div>
          <div className="keyboard">
-          <table class="keyboardTable">
+          <table className="keyboardTable">
             <tbody >
               <tr>
-                <td className="keyboardCol">1</td>
-                <td className="keyboardCol">2</td>
-                <td className="keyboardCol">3</td>
+                <td onClick={this.countChange} className="keyboardCol">1</td>
+                <td onClick={this.countChange} className="keyboardCol">2</td>
+                <td onClick={this.countChange} className="keyboardCol">3</td>
               </tr>
               <tr>
-                <td className="keyboardCol">4</td>
-                <td className="keyboardCol">5</td>
-                <td className="keyboardCol">6</td>
+                <td onClick={this.countChange} className="keyboardCol">4</td>
+                <td onClick={this.countChange} className="keyboardCol">5</td>
+                <td onClick={this.countChange} className="keyboardCol">6</td>
               </tr>
               <tr>
-                <td className="keyboardCol">7</td>
-                <td className="keyboardCol">8</td>
-                <td className="keyboardCol">9</td>
+                <td onClick={this.countChange} className="keyboardCol">7</td>
+                <td onClick={this.countChange} className="keyboardCol">8</td>
+                <td onClick={this.countChange} className="keyboardCol">9</td>
               </tr>
               <tr>
-                <td className="keyboardCol">0</td>
-                <td className="keyboardCol keyboardDelete" colSpan="2">DELETE</td>
+                <td onClick={this.countChange} className="keyboardCol">0</td>
+                <td onClick={this.countDelete} className="keyboardCol keyboardDelete" colSpan="2">DELETE</td>
               </tr>
             </tbody>
           </table>
@@ -39,22 +65,42 @@ class NumberKeyboard extends React.Component{
 }
 
 class Counter extends React.Component{
-  render(){
+  
+  state={
+    count:0,
+  }
+
+  decrease=()=>{
+    if(this.state.count>0){
+      const newCount=this.state.count-1;
+      this.setState({count:newCount});
+    }
+  }
+
+  increase=()=>{
+    if(this.state.count<9){
+      const newCount=this.state.count+1;
+      this.setState({count:newCount});
+    }
+  }
+
+
+  render(){  
     return(
       <div className="counter normal">
-        <div className="counterLaber"></div>
+        <div className="counterLaber">{this.props.operate}</div>
         <div className="counterContent">
-          <div className="counterOperate">
-            <svg class="am-icon am-icon-increase am-icon-md">
-
+          <div onClick={this.decrease} className="counterOperate">
+            <svg className="am-icon am-icon-increase am-icon-md">
+              <use xlinkHref="#decrease"></use>
             </svg>
           </div>
           <div className="counterNum">
-            <input type="text" pattern="[0-9]*" value="0" placeholder="Please Inupt" />
+            <input type="text" pattern="[0-9]*" value={this.state.count} placeholder="Please Inupt" />
           </div>
-          <div className="counterOperate">
-            <svg class="am-icon am-icon-increase am-icon-md">
-             <use xlinkHref="#back"></use>
+          <div onClick={this.increase} className="counterOperate">
+            <svg className="am-icon am-icon-increase am-icon-md">
+              <use xlinkHref="#increase"></use>
             </svg>
           </div>
         </div>
@@ -66,6 +112,10 @@ class Counter extends React.Component{
 
 class PartySize extends React.Component{
 
+  onFinish = () => {
+    
+  }
+  
   render(){
     return(
       <div className="global">
@@ -76,17 +126,19 @@ class PartySize extends React.Component{
             </div>
           </div>
           <div className="rightBtn">
-            <div className="button ghost">Next</div>
+            <div onClick={this.onFinish} className="button ghost">
+              <NavLink to="/phone">Next</NavLink>
+            </div>
           </div>
           <div className="headerContent">Party Size</div>
         </div>
         <div className="numberInput">
           <NumberKeyboard></NumberKeyboard>
           <div className="counterContainer">
-            <Counter></Counter>
-            <Counter></Counter>
-            <Counter></Counter>
-            <Counter></Counter>
+            <Counter ref="stroller" onRef={(el)=>this.stroller=el } operate="Stroller(s)"></Counter>
+            <Counter ref="highChair" operate="High Chair(s)"></Counter>
+            <Counter ref="wheelChair" operate="Wheel Chair(s)"></Counter>
+            <Counter ref="carSeat" operate="Car Seat(s)"></Counter>
           </div>
         </div>
       </div>
@@ -95,4 +147,4 @@ class PartySize extends React.Component{
 }
 
 
-export default PartySize
+export default PartySize;
--- src/componet/phone/Phone.js
+++ src/componet/phone/Phone.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import './Phone.css';
+import { NavLink } from 'react-router-dom' 
 
 class Phone extends React.Component{
+
+  state={
+    phoneNumber:''
+  }
+
+  phoneNuemberChange = (e) => {
+    if(this.state.phoneNumber.length<13){
+      if(this.state.phoneNumber.length==3 || this.state.phoneNumber.length==8){
+        const newPhoneNumber=this.state.phoneNumber+'-'+e.target.innerHTML
+        this.setState({phoneNumber:newPhoneNumber});  
+      }else{
+        const newPhoneNumber=this.state.phoneNumber+e.target.innerHTML
+        this.setState({phoneNumber:newPhoneNumber});  
+      }
+    }
+  }
+
+  numberDelete = (e) => {
+    if(this.state.phoneNumber.length>0){
+      const newPhoneNumber=this.state.phoneNumber.slice(0,[this.state.phoneNumber.length-1])
+      this.setState({phoneNumber:newPhoneNumber});
+    }
+  }
+
   render(){
     return(
       <div className="textGb">
@@ -14,11 +39,11 @@ class Phone extends React.Component{
                 <svg className="am-icon am-icon-back am-icon-md textColor">
                   <use xlinkHref="#back"></use>
                 </svg>
-                Back
+                <NavLink className="textColor" to="/">Back</NavLink>
               </a>
             </div>
-            <div className="stepNext textColor">
-              Done
+            <div onClick={this.onFinish} className="stepNext textColor">
+              <NavLink className="textColor" to="/">Done</NavLink>
               <svg className="am-icon am-icon-forward am-icon-md textColor">
                 <use xlinkHref="#forward"></use>
               </svg>
@@ -29,15 +54,14 @@ class Phone extends React.Component{
                   <svg className="am-icon am-icon-phone am-icon-md textColor">
                     <use xlinkHref="#phone"></use>
                   </svg>
-                  </div>
+                </div>
                 <div className="phoneShow">
                    <div className="phoneShowShadow">
                      000-0000-0000
                    </div>
                    <div className="inputPhoneShow">
-                      
-                   </div>
-          
+                      {this.state.phoneNumber}
+                   </div>        
                 </div>
               </div>
               <div className="separateLine"></div>
@@ -46,45 +70,45 @@ class Phone extends React.Component{
                   <tbody>
                     <tr>
                       <td className="keyBoardCol">
-                        <div className="keyboardColCircle">1</div>
+                        <div onClick={this.phoneNuemberChange} className="keyboardColCircle">1</div>
                       </td>
                       <td className="keyBoardCol">
-                        <div className="keyboardColCircle">2</div>
+                        <div onClick={this.phoneNuemberChange} className="keyboardColCircle">2</div>
                       </td>
                       <td className="keyBoardCol">
-                        <div className="keyboardColCircle">3</div>
+                        <div onClick={this.phoneNuemberChange} className="keyboardColCircle">3</div>
                       </td>
                     </tr>
                     <tr>
                       <td className="keyBoardCol">
-                        <div className="keyboardColCircle">4</div>
+                        <div onClick={this.phoneNuemberChange} className="keyboardColCircle">4</div>
                       </td>
                       <td className="keyBoardCol">
-                        <div className="keyboardColCircle">5</div>
+                        <div onClick={this.phoneNuemberChange} className="keyboardColCircle">5</div>
                       </td>
                       <td className="keyBoardCol">
-                        <div className="keyboardColCircle">6</div>
+                        <div onClick={this.phoneNuemberChange} className="keyboardColCircle">6</div>
                       </td>
                     </tr>
                     <tr>
                       <td className="keyBoardCol">
-                        <div className="keyboardColCircle">7</div>
+                        <div onClick={this.phoneNuemberChange} className="keyboardColCircle">7</div>
                       </td>
                       <td className="keyBoardCol">
-                        <div className="keyboardColCircle">8</div>
+                        <div onClick={this.phoneNuemberChange} className="keyboardColCircle">8</div>
                       </td>
                       <td className="keyBoardCol">
-                        <div className="keyboardColCircle">9</div>
+                        <div onClick={this.phoneNuemberChange} className="keyboardColCircle">9</div>
                       </td>
                     </tr>
                     <tr>
                       <td className="keyBoardCol">
-                        <div className="keyboardColCircle"></div>
+                        <div onClick={this.phoneNuemberChange} className="keyboardColCircle"></div>
                       </td>
                       <td className="keyBoardCol">
-                        <div className="keyboardColCircle">0</div>
+                        <div onClick={this.phoneNuemberChange} className="keyboardColCircle">0</div>
                       </td>
-                      <td className="keyBoardCol keyboardDelete">
+                      <td onClick={this.numberDelete} className="keyBoardCol keyboardDelete">
                         <div className="keyboardColCircle">
                           <svg className="am-icon am-icon-delete am-icon-md keyboardDeleteIcon">
                             <use xlinkHref="#delete"></use>
